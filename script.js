eaaf8b893f2f987cb54d2c305ee1ae2ebf77261f
KipshidzeNikoloz <kipshidzenikoloz@gmail.com>
2021-10-30 04:17:02 +0400

    added rainbow, black, white and grey (OPACITY LEFT TO FIGURE OUT), grid size increased to max of 100x100, styling left)

--- script.js
+++ script.js
@@ -1,51 +1,122 @@
-const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
 const resetButton = document.querySelector('.resetButton');
 const slider = document.querySelector('.slider');
 const sliderChoice = document.getElementById('gridNumber')
+const colorButtons = document.querySelectorAll('.colorButton');
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', chooseColor));
+var colorChoice = 'black';
 
 
+//Active
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', buttonActive))
 
+function buttonActive(e){
+    for (i = 0; i < colorButtons.length; i++) {
+        if (colorButtons[i].classList.contains('active')) 
+        colorButtons[i].classList.remove('active');
+    } 
+    this.classList.add('active')
+};
 
+//Grid
+function createGrid(){
+    for (i = 0; i < sliderToGridSize(); i++) {
+        var board = document.createElement('div');
+        board.className = 'board';
+        grid.appendChild(board);
+        grid.style.gridTemplateColumns = `repeat(${slider.value}, auto)`
+    }  
+        var gridBoxes = grid.querySelectorAll('div')
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', giveColor));
+}
 
-for (i = 0; i < 16; i++) {
-    var board = document.createElement('div');
-    board.className = 'board';
-    container.appendChild(board);
+createGrid();
+
+function deleteGrid(){
+        grid.replaceChildren()
+        grid.className = "grid";
 }
 
 
+//Coloring 
 
-container.addEventListener('mouseover', function (e) {
-      e.target.classList.add("hovered")
-}, false);  
-
-
-
-//Slider 
-
-sliderChoice.textContent = valueToGridNumber(slider.value); 
-
-slider.oninput = function() {
-    sliderChoice.textContent = valueToGridNumber(this.value);
+function giveColor(){
+    switch (colorChoice){ 
+        case 'rainbow':
+            this.style.background = generateRandomColor();
+            break;
+        case 'black':
+            this.style.background = "black";
+            break;
+        case 'white':
+            this.style.background = "white";
+            break;
+        case 'grey':
+            //figure out how to add opacity by 10% each hover
+        default: 
+            this.style.background = colorChoice;
+            break;
+    }   
 }
 
-function valueToGridNumber(num) {
-    const valueGridNumber = ['1x1', '2x2', '3x3', '4x4', '5x5', '6x6', '7x7', '8x8', '9x9', '10x10']
-    return valueGridNumber[num-1];
+
+//Color choice
+
+function chooseColor(e){
+    switch(e.target.dataset.color) {
+        case 'black':
+            colorChoice = 'black';
+            break;
+        case 'rainbow':
+            colorChoice = 'rainbow';
+            break;     
+        case 'white':
+            colorChoice = 'white'
+            break;
+        case 'grey':
+            colorChoice = 'grey'
+            break;
+        default: 'black'
+            colorChoice = 'black';
+            break;
+    }
 }
+
 
 //Reset
 
-resetButton.addEventListener('click', function(e) {
-    const resetBoard = document.querySelectorAll('.board.hovered');
-    for (i = 0; i < resetBoard.length; i++) {
-        resetBoard[i].classList.remove("hovered")
+function resetGrid() {
+    let gridBoxes = grid.querySelectorAll('div');
+    for (i = 0; i < gridBoxes.length; i++) {
+        gridBoxes[i].style.background = 'grey'
     }
-})
+}
 
-//Grid change
+resetButton.addEventListener('click', resetGrid)
 
-    // timeout function 
-    // setTimeout(function(){
-    //    e.target.style.background = "grey";
-    // }, 500);
+//Slider 
+
+var initialGridName = slider.value.concat('x', slider.value)
+sliderChoice.textContent = initialGridName;
+
+slider.oninput = function(){
+    var gridName = this.value.concat('x', this.value)
+    sliderChoice.textContent = gridName;
+    deleteGrid();
+    createGrid();
+    resetGrid();
+}
+
+
+//Grid size
+
+function sliderToGridSize() {
+    return slider.value * slider.value; 
+}
+
+// Random Color
+
+function generateRandomColor() {
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;  
+    }
